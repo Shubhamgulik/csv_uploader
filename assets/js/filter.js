b9a3100e37e36a4be63152567c45e8e78d29ab85
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2023-06-30 19:59:06 +0530

    Updated codebase

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,28 +1,31 @@
-console.log("hello");
+// When content is loaded event listener
 
-document.addEventListener('DOMContentLoaded', ()=> {
-	var searchBox = document.getElementById("searchBox");
-searchBox.addEventListener("keyup",function(){
-	var keyword = this.value;
-	keyword = keyword.toUpperCase();
-	var table = document.getElementById("table");
-	var all_tr = document.getElementsByTagName("tr");
-	for(var i=0; i<all_tr.length; i++){
-			var all_columns = all_tr[i].getElementsByTagName("td");
-		  for(j=0;j<all_columns.length; j++){
-				if(all_columns[j]){
-					var column_value = all_columns[j].textContent || all_columns[j].innerText;
-					
-					column_value = column_value.toUpperCase();
-					if(column_value.indexOf(keyword) > -1){
-						all_tr[i].style.display = ""; // show
-						break;
-					}else{
-						all_tr[i].style.display = "none"; // hide
-					}
-				}
-			}
-		}
-});
-});
+document.addEventListener("DOMContentLoaded", () => {
+  var searchBox = document.getElementById("searchBox");
 
+  // Evendt listener for Search Box
+
+  searchBox.addEventListener("keyup", function () {
+    var keyword = this.value;
+    keyword = keyword.toUpperCase();
+    var table = document.getElementById("table");
+    var all_tr = document.getElementsByTagName("tr");
+    for (var i = 0; i < all_tr.length; i++) {
+      var all_columns = all_tr[i].getElementsByTagName("td");
+      for (j = 0; j < all_columns.length; j++) {
+        if (all_columns[j]) {
+          var column_value =
+            all_columns[j].textContent || all_columns[j].innerText;
+
+          column_value = column_value.toUpperCase();
+          if (column_value.indexOf(keyword) > -1) {
+            all_tr[i].style.display = ""; // show
+            break;
+          } else {
+            all_tr[i].style.display = "none"; // hide
+          }
+        }
+      }
+    }
+  });
+});
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-async function  connect(){
-   await mongoose.connect('mongodb+srv://amit123:amit123@csvupload.cx0rkhg.mongodb.net/?retryWrites=true&w=majority');
+async function connect() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/csv-uploader");
 }
 connect();
 
 const db = mongoose.connection;
 
-db.on('err', console.error.bind(console, "Error in connecting to the database"));
+db.on(
+  "err",
+  console.error.bind(console, "Error in connecting to the database")
+);
 
-db.once("open", function(){
-    console.log("Connected to database :: Mongodb");
+db.once("open", function () {
+  console.log("Connected to database :: Mongodb");
 });
 
 module.exports = db;
